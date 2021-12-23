288eca34a0c924ef08c94bf2a3d7fbb57a5ea218
Mark Fullmer <mfullmer@gmail.com>
2021-12-23 07:13:30 -0600

    Add new mantras

--- data.js
+++ data.js
@@ -2,24 +2,38 @@ var data = {
   "name": "Guiding principles",
   "children": [
     {
-      "name": "Avoid the proximity bias",
+      "name": "Ego",
       "children": [
         {
-          "name": "People don't hear what you think you said."
+          "name": "Self doubt",
+          "children": [
+            {
+              "name": "You are your own worst critic",
+            },
+            {
+              "name": "Good work requires second-guessing initial choices",
+            }
+          ]
         },
         {
-          "name": "What you remember is not the same as what happened."
+          "name": "People don't hear what you think you said"
+        },
+        {
+          "name": "What you remember is not the same as what happened"
         }
       ]
     },
     {
-      "name": "Time, not money",
+      "name": "Time",
       "children": [
         {
-          "name": "Taking thing slower always makes it better."
+          "name": "Taking thing slower always makes it better"
         },
         {
-          "name": "The amount of time we designate to most tasks is not proportional to the value we ascribe to them."
+          "name": "The amount of time we designate to most tasks is not proportional to the value we ascribe to them"
+        },
+        {
+          "name": "Avoiding the tyranny of the urgent requires proactive intent"
         }
       ]
     },
@@ -27,21 +41,18 @@ var data = {
       "name": "Mindfulness",
       "children": [
         {
-          "name": "Things are not as [insert adjective] as they seem in the moment."
+          "name": "Things are not as [insert adjective] as they seem in the moment"
         },
         {
-          "name": "Assume good intentions."
+          "name": "Assume good intentions (or, at the very least, not malovent intent)"
         }
       ]
     },
     {
-      "name": "Manufactured problems.",
+      "name": "Manufactured problems",
       "children": [
         {
           "name": "\"Well, something'll happen.\" *1"
-        },
-        {
-          "name": "We are our own worst critic."
         }
       ]
     }
